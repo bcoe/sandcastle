2fa4bde45b9277834a8395c121021c756a2db250
bcoe <=>
2012-12-23 15:37:42 -0800

    continue to work on a thorough benchmarking example.

--- examples/example.js
+++ examples/example.js
@@ -1,24 +1,32 @@
-var SandCastle = require('../lib').SandCastle;
+var SandCastle = require('../lib').SandCastle,
+  equal = require('assert').equal;
 
 var sandcastle = new SandCastle({
-  api: './examples/api.js' // We provide an API with the setTimeout function.
+  api: './examples/api.js', // We provide an API with the setTimeout function.,
+  timeout: 1000
 });
 
-// A script that executes after 1 second.
+// Keep track of execution stats.
+var stats = {
+  script1: 0,
+  script2: 0,
+  script3: 0
+};
+
+// A non-malicious script with a timeout.
 var script1 = sandcastle.createScript("\
   exports.main = function() {\
     setTimeout(function() {\
       exit('script1 complete')\
-    }, 1000)\
+    }, 250)\
   }\
 ");
 
 script1.on('exit', function(err, output) {
-  console.log(output);
+  stats.script1 += 1;
 });
 
 script1.on('timeout', function() {
-  console.log('rescheduling script 1');
   script1.run();
 });
 
@@ -30,51 +38,54 @@ var script2 = sandcastle.createScript("\
 ");
 
 script2.on('exit', function(err, output) {
-  console.log(output);
+  stats.script2 += 1;
 });
 
-script2.on('timeout', function() {
-  console.log('not rescheduing 2 it is malicious.')
-});
-
-// a script that exists after 2 seconds.
+// A non-malicious script with a timeout.
 var script3 = sandcastle.createScript("\
   exports.main = function() {\
     setTimeout(function() {\
       exit('script3 complete')\
-    }, 2000)\
+    }, 500)\
   }\
 ");
 
 script3.on('exit', function(err, output) {
-  console.log(output);
+  stats.script3 += 1;
 });
 
 script3.on('timeout', function() {
-  console.log('rescheduling script 3');
+  script3.removeAllListeners('exit');
+  script3.on('exit', function(err, output) {
+    stats.script3 += 1;
+    
+    // Make sure the scripts were
+    // executed the appropriate # of times.
+    equal(stats.script1, 2);
+    equal(stats.script2, 0);
+    equal(stats.script3, 2);
+    console.log('scripts executed the correct # of times', stats)
+
+    process.exit(0);
+  });
   script3.run();
 });
 
 // Running script 1 and 3 will take.
 // about 2 seconds to complete.
-console.log('running script 1')
 script1.run();
-console.log('running script 2')
 script3.run();
 
 var id = setInterval(function() {
   if (script1.exited && script3.exited) {
     clearInterval(id);
 
-    console.log('both script1 and script 2 exited.')
-    console.log('script2 is malicious and will cause the sandbox to shutdown');
-    // Running script 1 and 3 will take.
-    // about 2 seconds to complete.
-    console.log('running script 1')
-    script1.run();
-    console.log('running script 2')
-    script2.run();
-    console.log('running script 3')
+    // both script1 and script 2 exited. after their first execution.
+    // next we will schedule them with a malicious script.
+
     script3.run();
+    script1.run();
+    // script2 is malicious and will cause the sandbox to shutdown'
+    script2.run();
   }
 }, 1000);
