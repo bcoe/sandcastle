6000508faf5a892311b5f051f20cb29cc5f1c004
Owen Evans <owen@bgeek.net>
2014-07-14 15:55:45 +1200

    allow trusted api to access the globals

---
+++ examples/contextObjectApi.js
@@ -0,0 +1,11 @@
+
+var StateManager = function(){
+	var state = contextObject.state;
+	delete contextObject.state;
+	this.getState=function(){
+		return state;
+	};
+};
+exports.api = {
+  stateManager : new StateManager()
+};
